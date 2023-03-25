9a5ca23f6548525bf404618b815de6c17629047c
jennifergri <jennifergr@hotmail.no>
2023-03-25 01:33:44 +0100

    App.js og Layout.js
    Oppsett og overskrift på nettsiden

---
+++ arbeidskrav/src/components/Layout.js
@@ -0,0 +1,13 @@
+export default function Layout(){
+    return(
+        <div className="container">
+            <header>
+                <h1>Filmsøk</h1>
+            </header>
+            
+            <main>
+
+            </main>
+        </div>
+    )
+}

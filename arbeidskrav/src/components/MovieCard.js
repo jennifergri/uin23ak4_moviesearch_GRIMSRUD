33dd6cb439d8f0b6cc0654841df05b5afc480b6e
jennifergri <jennifergr@hotmail.no>
2023-03-25 01:12:53 +0100

    MovieCard

--- arbeidskrav/src/components/MovieCard.js
+++ arbeidskrav/src/components/MovieCard.js
@@ -1,3 +1,10 @@
-export default function MovieCard(){
-    return
+export default function MovieCard({img, title, ingress}){
+    return(
+        <article className="article-card">
+            <img src={img} alt={title}></img>
+            <h3>{title}</h3>
+            <p>{ingress}</p>
+            <a href="#">Les mer</a>
+        </article>
+    )
 }

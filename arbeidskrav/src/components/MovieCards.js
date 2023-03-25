93956f3cbbc1f84c289da624035363872bd93c2d
jennifergri <jennifergr@hotmail.no>
2023-03-25 01:50:08 +0100

    endret API

--- arbeidskrav/src/components/MovieCards.js
+++ arbeidskrav/src/components/MovieCards.js
@@ -3,9 +3,10 @@ import { useEffect, useState } from "react"
 export default function MovieCards(){
     
     const [movies, setMovies] = useState([])
+    const [search, setSearch] = useState('james-bond') /*Default på forsiden*/
     
     const getMovies = async() => {
-        const response = await fetch ('http://www.omdbapi.com/?q=james-bond&pageSize=10&i=tt3896198&apikey=9ebf19a7')
+        const response = await fetch (`http://www.omdbapi.com/?q=${search}&pageSize=10&i=tt3896198&apikey=9ebf19a7`)
         const data = await response.json()
         setMovies(data)
         console.log(data)

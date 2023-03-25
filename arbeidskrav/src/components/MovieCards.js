a87be80db51d75d417295bf9cdf8c8837d5e2a73
jennifergri <jennifergr@hotmail.no>
2023-03-25 01:26:38 +0100

    API
    start

--- arbeidskrav/src/components/MovieCards.js
+++ arbeidskrav/src/components/MovieCards.js
@@ -1,3 +1,19 @@
+import { useEffect, useState } from "react"
+
 export default function MovieCards(){
+    
+    const [movies, setMovies] = useState([])
+    
+    const getMovies = async() => {
+        const response = await fetch ('http://www.omdbapi.com/?q=james-bond&pageSize=10&i=tt3896198&apikey=9ebf19a7')
+        const data = await response.json()
+        setMovies(data)
+        console.log(data)
+    }
+
+    useEffect(() => {
+        getMovies()
+    },[])
+
     return
 }

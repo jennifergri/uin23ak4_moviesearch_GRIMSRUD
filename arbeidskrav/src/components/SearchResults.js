c09735f2b1e118d3194ad0d0ae1a839170537a22
jennifergri <jennifergr@hotmail.no>
2023-03-27 11:59:50 +0200

    fungerende søkefelt

--- arbeidskrav/src/components/SearchResults.js
+++ arbeidskrav/src/components/SearchResults.js
@@ -1,14 +1,21 @@
 import React from 'react';
-export default function SearchResults({getMovies}){
+export default function SearchResults({getMovies, setSearch}){
     
     //funksjon som gjør at siden ikke skal refreshe seg:
     const handleSubmit = (event) => {
         event.preventDefault()
     }
+
+    const handleSearch = (event) => {
+        if(event.target.value.length > 2) {
+            setSearch(event.target.value)
+            console.log(event.target.value)
+        }
+    }
     
     return(
         <form onSubmit={handleSubmit}>
-            <input type="search" placeholder="søk etter film..."/>
+            <input type="search" placeholder="Søk etter film..." onChange={handleSearch}/>
             <button type="submit" onClick={getMovies}>Søk</button>
         </form>
     )
